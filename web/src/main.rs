c5764bcc9b855420f9d6ad0833fcd1de740d9feb
a-isaiahharvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-06-09 17:25:14 -0400

    Add support for copying to clipboard

--- web/src/main.rs
+++ web/src/main.rs
@@ -8,16 +8,6 @@ use web_sys::MouseEvent;
 fn RandomColorGenerator(cx: Scope) -> impl IntoView {
     let (color, set_color) = create_signal(cx, rand::random::<Color>());
 
-    let onclick_generate_button = move |_: MouseEvent| {
-        set_color(rand::random::<Color>());
-        document()
-            .body()
-            .unwrap()
-            .style()
-            .set_property("background", color().to_string().as_str())
-            .unwrap();
-    };
-
     document()
         .body()
         .unwrap()
@@ -32,7 +22,26 @@ fn RandomColorGenerator(cx: Scope) -> impl IntoView {
                     <span class="color">{color().to_string()}</span>
                 }
             }</h2>
-            <button class="btn btn-hero" on:click=onclick_generate_button>"Generate"</button>
+            <div class="flex justify-center space-x-4">
+                <button class="btn btn-hero" on:click=move |_: MouseEvent| {
+                    set_color(rand::random::<Color>());
+                    document()
+                        .body()
+                        .unwrap()
+                        .style()
+                        .set_property("background", color().to_string().as_str())
+                        .unwrap();
+                }>"Generate"</button>
+                <button class="btn btn-hero" on:click=move |_: MouseEvent| {
+                    let navigator = window().navigator();
+
+                    let _ = navigator
+                        .clipboard()
+                        .unwrap()
+                        .write_text(color().to_string().as_str());
+                }>"Copy"</button>
+            </div>
+
         </div>
     }
 }
